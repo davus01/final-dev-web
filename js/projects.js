11efd16a8e6833c00c0b13097a306cce158ed48a
David Thomas <david.thomas@ensea.fr>
2024-02-05 22:38:42 +0100

    Premier commit

---
+++ js/projects.js
@@ -0,0 +1,52 @@
+const projects = [
+	{
+		title: "Python Flask",
+		description: "lorem ipsum numero uno",
+		imageUrl: "flask-api.png",
+		technologies: ["Python", "Restful API"],
+		links: [
+			"https://replit.com/@MartinPedraza/basic-rest-flask#main.py",
+			"https://basic-rest-flask.martinpedraza.repl.co/",
+		],
+	},
+	{
+		title: "Landing Page",
+		description: "lorem ipsum numero dos",
+		imageUrl: "front-01.png",
+		technologies: ["Html", "Css"],
+		links: [
+			"https://github.com/web-rest-api/tp1-site",
+			"https://web-rest-api.github.io/tp1-site/",
+		],
+	},
+	{
+		title: "Game Manager",
+		description: "lorem ipsum numero tres",
+		imageUrl: "game-manager.png",
+		technologies: ["Bootstrap 5", "Javascript"],
+		links: [
+			"https://github.com/web-rest-api/tp-1-bootstrap",
+			"https://web-rest-api.github.io/tp-1-bootstrap/",
+		],
+	},
+	{
+		title: "Favorite Books",
+		description: "lorem ipsum numero tres",
+		imageUrl: "favorite-books.png",
+		technologies: ["Bootstrap 5", "Javascript", "Resful API"],
+		links: [
+			"https://github.com/web-rest-api/tp3-bootstrap-api",
+			"https://web-rest-api.github.io/tp4-bootstrap-api/",
+		],
+	},
+	{
+		title: "Users",
+		description: "Uthentication API",
+		imageUrl: "nodeJs-express.png",
+		technologies: ["NodeJs", "Express"],
+		links: [
+			"https://replit.com/@6thomasdavid/mainpy#main.py",
+			"https://node-expess.martinpedraza.repl.co/",
+		],
+	},
+]
